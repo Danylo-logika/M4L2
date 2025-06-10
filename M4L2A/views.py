1c141557e5b3c3e05964db172a41d67516a5fd1d
BossDan9I <danylo.logika@gmail.com>
2025-06-11 00:07:00 +0300

    add Html and css

--- M4L2A/views.py
+++ M4L2A/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from M4L2A.models import Room, Booking
+
 # Create your views here.
 def main_page(request):
-
+    rooms = Room.objects.all()
     context = {
-        "data": "hello from Django"
+        "data": "hello from Django",
+        'room_list': rooms
     }
 
-    return render(request,"index.html", context)
+    return render(request,"M4L2A/room_list.html", context)
